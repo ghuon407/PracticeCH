3137729cea0664b84da453f72960b0d6e3d3c502
Garrett Huon <ghuon407@gmail.com>
2021-12-07 11:37:15 -0500

    new challenges

---
+++ fractions/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace fractions
+{
+    class Program
+    {
+        public static double UniqueFraq()
+        {
+            List<double> fraq = new List<double>();
+            List<double> nope = new List<double>();
+            for(int i =1; i < 10; i++)
+            {
+                for(int j = 1; j < 10; j++)
+                {
+                    double f = (double)i/j;
+                    bool check = fraq.Contains(f);
+                    if(f < 1 && check == false)
+                    {
+                        fraq.Add(f);
+                    }
+                    else if(f >= 1 || check == true)
+                    {
+                        nope.Add(f);
+                    }
+                    
+                }
+            }
+            double ans = fraq.Sum();
+            return Math.Round(ans,3);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(UniqueFraq());
+        }
+    }
+}
---
+++ wages/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace wages
+{
+    class Program
+    {
+        public static void WageCalc(double[] stats)
+        {
+            double start = stats[0];
+            double end = stats[1];
+            double rate = stats[2];
+            double or = stats[3];
+            double total;
+            double overtime;
+            if(end > 17)
+            {
+                overtime = end - 17;
+                double pay = (17-start)*rate;
+                double ot = overtime*rate*or;
+                total = pay + ot;
+            }
+            else
+            {
+                total = (end-start)*rate;
+            }
+            Console.WriteLine(total);
+        }
+        static void Main(string[] args)
+        {
+            double a = 9;
+            double b = 17;
+            double c = 30;
+            double d = 1.5;
+
+            double[] stats = {a, b, c, d};
+            WageCalc(stats);
+        }
+    }
+}
