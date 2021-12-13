91a69982f416b082381247da51b4f9dca35fc2a9
Garrett Huon <ghuon407@gmail.com>
2021-12-13 14:05:09 -0500

    new challenges

---
+++ palindrome/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace palindrome
+{
+    class Program
+    {
+        public static void palindromeTest(string input)
+        {
+            int num = input.Length;
+            int same = 0;
+            int dif = 0;
+            char[] reverse = input.ToCharArray();
+            Array.Reverse(reverse);
+            for(int i = 0; i< num; i++)
+            {
+
+                if(input[i] == reverse[i])
+                {
+                    same++;
+                    
+                }
+                else if(input[i] != reverse[i])
+                {
+                    dif++;
+
+                    
+                }
+            }
+            if(dif>2)
+            {
+                Console.WriteLine("False");
+            }
+            else if(dif == 2)
+            {
+                Console.WriteLine("True");
+            }
+            else{
+                Console.WriteLine("Is not almost Palindrome");
+            }
+        }
+        static void Main(string[] args)
+        {
+            string input = "racdcar";
+
+            palindromeTest(input);
+
+        }
+    }
+}
---
+++ stairs/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace stairs
+{
+    class Program
+    {
+        public static int stairWays(int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return 1;
+            }
+            else if( n ==2)
+            {
+                return 2;
+            }
+            else
+            {
+                return stairWays( n-2 ) + stairWays( n-1 );
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            int n = 4;
+            Console.WriteLine(stairWays(n));
+        }
+    }
+}
